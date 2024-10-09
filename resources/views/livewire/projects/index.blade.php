64a141623397ee4dfc5a007acf15dd74b9979160
Matheus Fernando Freire Ferreira <matheusfernandofreire@hotmail.com>
2024-10-09 18:20:52 -0300

    Version 1.0.0

---
+++ resources/views/livewire/projects/index.blade.php
@@ -0,0 +1,8 @@
+<div class="grid grid-cols-2 gap-4">
+    @foreach($this->projects as $project)
+    <a href="{{ route('projects.show', $project) }}">
+        <x-projects.card-simple :$project />
+    </a>
+
+    @endforeach
+</div>
